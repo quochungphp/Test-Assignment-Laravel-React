423822e5cd471aecbfa6aea7f4bd8766a93f7d01
LeQuocHung <quochungphp@gmail.com>
2021-02-17 17:59:28 +0700

    Config Laravel API and Return all of request use default view

---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\OrganisationController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+
+
+Route::middleware('session_api')->group(function () {
+
+  Route::get('/user/index', [UserController::class, 'index']);
+  Route::put('/user/update/{id?}', [UserController::class, 'update']);
+  Route::put('/user/create', [UserController::class, 'create']);
+  Route::delete('/user/delete/{id?}', [UserController::class, 'delete']);
+  Route::get('/user/get-info/{id?}', [UserController::class, 'get']);
+
+
+
+  Route::get('/organisation/index', [OrganisationController::class, 'index']);
+  Route::post('/organisation/save/{id?}', [OrganisationController::class, 'save']);
+  Route::delete('/organisation/delete/{id?}', [OrganisationController::class, 'delete']);
+  Route::get('/organisation/get-info/{id?}', [OrganisationController::class, 'get']);
+
+
+});
